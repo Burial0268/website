8ad1aa0517e547c67fb3b3196e47d30a86ce0f39
woshoxxx <84318882@qq.com>
2021-08-10 11:22:52 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { PageHeader, Button } from "antd";
+import "antd/dist/antd.css"
+
+
+
+function App() {
+  return (
+    <div>
+      <PageHeader
+        avatar={{ src: "https://cdn-pic.gcxstudio.cn/2021/07/05/5B8804572CA7725395EE48BEF3E1BD77.png" }}
+        title="GBCLStudio 官网">
+      </PageHeader>
+
+
+    </div>
+  );
+}
+
+export default App;
