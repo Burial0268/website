38da4d239da76864a9b44cdc5986a2ec86afa6cf
woshoxxx <84318882@qq.com>
2021-08-13 10:50:56 +0800

    feat(Carousel): add a Carousel in home.

---
+++ src/components/Products/index.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Carousel, Image } from "antd";
+
+class Products extends Component {
+    render() {
+        return (
+            <div>
+                <Carousel autoplay>
+                    <Image preview={false} src="https://gbclstudio.cn/p1.jpg"></Image>
+                    <Image preview={false} src="https://gbclstudio.cn/p2.jpg"></Image>
+                    <Image preview={false} src="https://gbclstudio.cn/p3.png"></Image>
+                </Carousel>
+            </div>
+        )
+    }
+}
+
+export default Products;
