e799391fcd99f4a714e1c1df0532c21450dfa03b
woshoxxx <84318882@qq.com>
2021-08-13 15:21:55 +0800

    chore: add link for Dinosaur Cloud.

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -2,17 +2,23 @@ import React, { Component } from "react";
 import { Carousel, Image } from "antd";
 
 class Products extends Component {
-    render() {
-        return (
-            <div>
-                <Carousel autoplay>
-                    <Image preview={false} src="https://gbclstudio.cn/p1.jpg"></Image>
-                    <Image preview={false} src="https://gbclstudio.cn/p2.jpg"></Image>
-                    <Image preview={false} src="https://gbclstudio.cn/p3.png"></Image>
-                </Carousel>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Carousel autoplay>
+          <Image preview={false} src="https://gbclstudio.cn/p1.jpg"></Image>
+          <Image preview={false} src="https://gbclstudio.cn/p2.jpg"></Image>
+          <Image
+            preview={false}
+            onClick={() => {
+              location.href = "https://www.gcxstudio.cn/";
+            }}
+            src="https://gbclstudio.cn/p3.png"
+          ></Image>
+        </Carousel>
+      </div>
+    );
+  }
 }
 
 export default Products;
