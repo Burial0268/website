d153e028d28189cb269ed86bf7e7cd472ef6aa25
woshoxxx <84318882@qq.com>
2021-08-13 15:03:57 +0800

    chore: add message tip to be mobile friendly.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
 import React from "react";
-import { PageHeader, Row, Col, message } from "antd";
+import { PageHeader, Row, Col, message, notification } from "antd";
 import "antd/dist/antd.css";
 import Products from "./components/Products";
-import MemberList from "./components/MemberList";;
+import MemberList from "./components/MemberList";
 import Footer from "./components/Footer";
 import AffixTip from "./components/AffixTip";
 import { useEffect } from "react";
 
-
 function App() {
   useEffect(() => {
-    message.info("当前网站处于未完工状态，当前样式不代表最终品质")
-  })
+    //message.info("当前网站处于未完工状态，当前样式不代表最终品质");
+    notification["info"]({
+      message: 'Tips',
+      description:
+        "我们欢迎您与我们交流，也希望你能加入我们，与我们一起携手共进！",
+    });
+  });
   return (
     <div>
       <Row>
         <Col span={16} offset={4}>
           <PageHeader
-            avatar={{ src: "https://cdn-pic.gcxstudio.cn/2021/07/05/5B8804572CA7725395EE48BEF3E1BD77.png" }}
-            title="GBCLStudio 官网">
-          </PageHeader>
+            avatar={{
+              src: "https://cdn-pic.gcxstudio.cn/2021/07/05/5B8804572CA7725395EE48BEF3E1BD77.png",
+            }}
+            title="GBCLStudio 官网"
+          ></PageHeader>
           <Products></Products>
           <MemberList></MemberList>
           <Footer></Footer>
         </Col>
       </Row>
       <AffixTip></AffixTip>
-    </div >
+    </div>
   );
 }
 
---
+++ src/components/AffixMenu/index.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Affix, Menu } from "antd";
+
+class AffixMenu extends Component {
+
+    render() {
+        const { SubMenu } = Menu
+        return (
+
+            <div>
+                <Affix offsetTop={10}>
+                    <Menu style={{ width: 20 }} title="" mode="vertical">
+                        <SubMenu key="0">
+                            <Menu.Item key="00">
+                                a
+                            </Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Affix>
+            </div >
+
+        )
+    }
+}
+
+export default AffixMenu;
